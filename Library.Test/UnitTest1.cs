1eeb53f55b3ffbdf861e1385f79e4031ee00eeb7
89catbjo <89catbjo@skola.boras.se>
2024-01-10 15:53:14 +0100

    Add Unittest

--- Library.Test/UnitTest1.cs
+++ Library.Test/UnitTest1.cs
@@ -19,4 +19,29 @@ public class UnitTest1
 
         Assert.True(booksaddedtolibrary.Count() == 1);
     }
+
+    [Fact]
+    public void SearchForBook_searchstring_ShouldReturnFoundBooks()
+    {
+        // arrange
+
+        Catalogue ca = new Catalogue();
+        ca.AddBook(new Book("saras äventyr", "kristina svensson", "2014"));
+        ca.AddBook(new Book("Harry Potter den vises sten", "j.k", "2015"));
+        ca.AddBook(new Book("Harry Potter - flammande bägaren", "jk", "2016"));
+
+        int expected = 2;
+
+        //act
+
+        List<Book> foundbooks = ca.SearchForBook("Harry");
+        int actual = foundbooks.Count();
+
+
+
+        // Assert.
+
+        Assert.Equal(expected, actual);
+
+    }
 }
